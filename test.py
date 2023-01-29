badf064d3d7712ce9e11176c3f83477201a1d733
Black-foot-ub <122695151+Black-foot-ub@users.noreply.github.com>
2023-01-29 14:42:07 +0300

    Update test.py

--- test.py
+++ test.py
@@ -4,5 +4,9 @@ from blackfoot import loader, utils
 class SomeTestMod(loader.Module):
     "TEst"
     
+    strings = {
+        "name": "LGBTGovno"
+    }
+    
     async def somecmd(self, client, message):
         await message.delete()
